5cccf7975b4087ae961231ffb7ed16c0724a1aa6
DLun1971 <dwainlunau@icloud.com>
2026-03-19 15:52:00 -0500

    sl: fix BT section name corruption

--- data/sl-data.js
+++ data/sl-data.js
@@ -249,7 +249,8 @@ const SL_CATEGORIES = [
   },
   {
     id: 12,
-    name: ,
+    name: 'BT/Ops Critical Wireless',
+    items: [
       // OC Wireless replacement parts
       { desc: 'Replacement Wireless Earpiece 12 inch Cable (for NNTN8125)', part: 'NTN2572', ear_type: null, mic_type: null, ptt: null, talk_time_hr: null, charge_time_hr: null, weight_g: null, variants: ['7000','7000e'] },
       { desc: 'Wireless Earpiece Maintenance Kit (10 silicone tubes, 10 mic seal strips, 2 foam windscreens)', part: 'NTN8821', ear_type: null, mic_type: null, ptt: null, talk_time_hr: null, charge_time_hr: null, weight_g: null, variants: ['7000','7000e'] },
